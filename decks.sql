de7b689efa9ed3724403831dfd65493bba215512
shingoed <nakajima.shingo@gmail.com>
2019-12-27 16:53:01 -0800

    day2 tasks done saving cards to database

--- decks.sql
+++ decks.sql
@@ -4,7 +4,11 @@ CREATE TABLE IF NOT EXISTS collection (
   name VARCHAR(255),
   date VARCHAR(255),
   image_url TEXT,
-  legal VARCHAR(255),
+  legal0 VARCHAR(255),
+  legal1 VARCHAR(255),
+  legal2 VARCHAR(255),
+  legal3 VARCHAR(255),
+  legal4 VARCHAR(255),
   tag VARCHAR(255)
 );
 
