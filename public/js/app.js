703a58d0de070aba0b896eb6c51687d0d5212242
Brandon Johnson <splintercel3000@gmail.com>
2020-01-02 11:15:13 -0800

    testing button colors

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,57 @@
 'use strict';
 
+$('#search-button').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#search-button').on('mouseup',function(){
+    $(this).css("background-color", "rgb(255, 157, 0)");
+});
+
+$('#collection').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#collection').on('mouseup',function(){
+    $(this).css("background-color", "rgb(255, 157, 0)");
+});
+
+$('#aboutus').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#aboutus').on('mouseup',function(){
+    $(this).css("background-color", "rgb(255, 157, 0)");
+});
+
+$('#wish-list').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#wish-list').on('mouseup',function(){
+    $(this).css("background-color", "rgb(255, 157, 0)");
+});
+
+$('#home-button').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#home-button').on('mouseup',function(){
+    $(this).css("background-color", "rgb(255, 157, 0)");
+});
+
+$('#delete-button').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#delete-button').on('mouseup',function(){
+    $(this).css("background-color", "rgb(207, 10, 10)");
+});
+
+$('#delete-all-button').on('mousedown',function(){
+    $(this).css("background-color", "blue");
+});
+
+$('#delete-all-button').on('mouseup',function(){
+    $(this).css("background-color", "rgb(207, 10, 10)");
+});
